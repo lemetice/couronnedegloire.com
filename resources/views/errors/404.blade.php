baa295250098180a323abc4a8612cc8faaf65b69
Arnold <arnoldtagne@gmail.com>
2017-10-13 04:25:08 +0530

    Added Error 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #C71B1B;
+				display: table;
+				font-weight: 100;				
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 150px;
+				margin-bottom: 40px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<p> Page non trouv&eacute;e</p>	
+				<div class="title">404 !</div>		
+			</div>
+
+			<div class="">Que Dieu vous b&eacute;nisse!</div>
+
+		</div>
+	</body>
+</html>
