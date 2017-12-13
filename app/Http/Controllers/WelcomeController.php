de3191cb9f0124a24031aa812f40c907728efe3f
Arnold <arnoldtagne@gmail.com>
2017-12-13 20:53:34 +0530

    Success flash message-contactus

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -101,8 +101,11 @@ class WelcomeController extends Controller {
 
 		$success_message ='<span style="background-color: #A7C139; border-radius: 10px; -webkit-border-radius: 8px; color: #fff; padding: 0px 10px;">Thanks for contacting us. You will get a reply within 24hrs</span>';
 
-		
-		return view('contacts', compact('success_message'));
+		Session::flash('success_message', '
+				        Merci de nous avoir contacté. Vous obtiendrez une réponse dans moins de 24 heures'
+				       );
+
+		return view('contacts');
 	}
 
 }
