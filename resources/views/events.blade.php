9384370b8a841ea00cf6c6104c95edf19b9ca58c
Arnold <arnoldtagne@gmail.com>
2018-05-05 21:32:12 +0530

    Events update

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -36,7 +36,7 @@
           <div class="col-md-12">
             <ul class="grid-holder col-3 events-grid isotope" style="position: relative; overflow: hidden; height: 1040.34px;">
                 <li class="grid-item format-standard isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate(0px, 0px);">
-                  <div class="grid-item-inner"> <a href="#" class="media-box"> <img src="{{ asset('/images/gallery-img3.jpg') }}" alt=""> <span class="zoom" style="width: 224px; height: 224px; line-height: 224px;"><i class="fa fa-plus"></i></span></a>
+                  <div class="grid-item-inner"> <a href="#" class="media-box"> <img src="{{ asset('/images/ecole-du-saint-esprit.jpeg') }}" alt=""> <span class="zoom" style="width: 224px; height: 224px; line-height: 224px;"><i class="fa fa-plus"></i></span></a>
                     <div class="grid-content">
                       <h3><a href="#">L'école du Saint esprit</a></h3>
                       <p>Nous commençons l'école du Saint esprit à partir du 1er janvier 2018. Les inscriptions sont ouvertes
@@ -49,10 +49,14 @@
                   </div>
                 </li>
                 <li class="grid-item format-standard isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate(0px, 0px);">
-                  <div class="grid-item-inner"> <a href="#" class="media-box"> <img src="{{ asset('/images/gallery-img34.jpg') }}" alt=""> <span class="zoom" style="width: 224px; height: 224px; line-height: 224px;"><i class="fa fa-plus"></i></span></a>
+                  <div class="grid-item-inner"> <a href="#" class="media-box"> <img src="{{ asset('/images/evenement de mai.jpeg') }}" alt=""> <span class="zoom" style="width: 224px; height: 224px; line-height: 224px;"><i class="fa fa-plus"></i></span></a>
                     <div class="grid-content">
                       <h3><a href="#">Conférences annuelle</a></h3>
-                      <p>Conférences annuelle  de M.E.I.C.G en fin mai 2018
+                      <p>Conférences annuelle  de M.E.I.C.G du 25 au 26 mai 2018
+                      </p>
+                      <p>Vendredi 25 Mai  de 16h à 20h
+                      </p>
+                      <p>Samedi 26 Mai  de 14h à 18h
                       </p>
                     </div>
                     <ul class="info-table">
@@ -70,6 +74,18 @@
                     </div>
                     <ul class="info-table">
                       <li><i class="fa fa-calendar"></i>08 mars 2018 </li>
+                      <li><i class="fa fa-map-marker"></i> 93200 Saint-Denis RER D, FR</li>
+                    </ul>
+                  </div>
+                </li>
+                <li class="grid-item format-standard isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate(0px, 0px);">
+                  <div class="grid-item-inner"> <a href="#" class="media-box"> <img src="{{ asset('/images/gallery-img34.jpg') }}" alt=""> <span class="zoom" style="width: 224px; height: 224px; line-height: 224px;"><i class="fa fa-plus"></i></span></a>
+                    <div class="grid-content">
+                      <h3><a href="#">Anniversaire de la M.E.I.C.G</a></h3>
+                      <p>28 Juillet 2018</p>
+                    </div>
+                    <ul class="info-table">
+                      <li><i class="fa fa-calendar"></i>Juillet 2018 </li>
                       <li><i class="fa fa-map-marker"></i> 93200 Saint-Denis RER D, FR</li>
                     </ul>
                   </div>
